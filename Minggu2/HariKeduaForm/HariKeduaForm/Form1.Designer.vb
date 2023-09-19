86411f3a7057c2721a95b6476a30262c36bbddc6
Bimo Indracahya <bimo.17168@gmail.com>
2023-09-19 11:26:15 +0700

    Fix Folder

---
+++ Minggu2/HariKeduaForm/HariKeduaForm/Form1.Designer.vb
@@ -0,0 +1,326 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Input1 = New System.Windows.Forms.Label()
+        Me.Input2 = New System.Windows.Forms.Label()
+        Me.Input3 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Result = New System.Windows.Forms.Label()
+        Me.ResultLabel = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SK1 = New System.Windows.Forms.Button()
+        Me.SK2 = New System.Windows.Forms.Button()
+        Me.SK3 = New System.Windows.Forms.Button()
+        Me.SK4 = New System.Windows.Forms.Button()
+        Me.SK5 = New System.Windows.Forms.Button()
+        Me.SK6 = New System.Windows.Forms.Button()
+        Me.SK12 = New System.Windows.Forms.Button()
+        Me.SK11 = New System.Windows.Forms.Button()
+        Me.SK10 = New System.Windows.Forms.Button()
+        Me.SK9 = New System.Windows.Forms.Button()
+        Me.SK8 = New System.Windows.Forms.Button()
+        Me.SK7 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Input1
+        '
+        Me.Input1.AutoSize = True
+        Me.Input1.Location = New System.Drawing.Point(38, 33)
+        Me.Input1.Name = "Input1"
+        Me.Input1.Size = New System.Drawing.Size(42, 16)
+        Me.Input1.TabIndex = 0
+        Me.Input1.Text = "Input1"
+        '
+        'Input2
+        '
+        Me.Input2.AutoSize = True
+        Me.Input2.Location = New System.Drawing.Point(38, 74)
+        Me.Input2.Name = "Input2"
+        Me.Input2.Size = New System.Drawing.Size(42, 16)
+        Me.Input2.TabIndex = 1
+        Me.Input2.Text = "Input2"
+        '
+        'Input3
+        '
+        Me.Input3.AutoSize = True
+        Me.Input3.Location = New System.Drawing.Point(38, 115)
+        Me.Input3.Name = "Input3"
+        Me.Input3.Size = New System.Drawing.Size(42, 16)
+        Me.Input3.TabIndex = 2
+        Me.Input3.Text = "Input3"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(128, 30)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(525, 22)
+        Me.TextBox1.TabIndex = 3
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(128, 71)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(525, 22)
+        Me.TextBox2.TabIndex = 4
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(128, 112)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(525, 22)
+        Me.TextBox3.TabIndex = 5
+        '
+        'Result
+        '
+        Me.Result.AutoSize = True
+        Me.Result.Location = New System.Drawing.Point(38, 159)
+        Me.Result.Name = "Result"
+        Me.Result.Size = New System.Drawing.Size(51, 16)
+        Me.Result.TabIndex = 6
+        Me.Result.Text = "Result :"
+        '
+        'ResultLabel
+        '
+        Me.ResultLabel.AutoSize = True
+        Me.ResultLabel.Location = New System.Drawing.Point(125, 159)
+        Me.ResultLabel.Name = "ResultLabel"
+        Me.ResultLabel.Size = New System.Drawing.Size(10, 16)
+        Me.ResultLabel.TabIndex = 7
+        Me.ResultLabel.Text = " "
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(128, 222)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(86, 36)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "IF#1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(256, 222)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(86, 36)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "IF#2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(395, 222)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(86, 36)
+        Me.Button3.TabIndex = 10
+        Me.Button3.Text = "IF#3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(532, 222)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(151, 36)
+        Me.Button4.TabIndex = 11
+        Me.Button4.Text = "Select Case"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'SK1
+        '
+        Me.SK1.Location = New System.Drawing.Point(117, 284)
+        Me.SK1.Name = "SK1"
+        Me.SK1.Size = New System.Drawing.Size(88, 45)
+        Me.SK1.TabIndex = 12
+        Me.SK1.Text = "#1"
+        Me.SK1.UseVisualStyleBackColor = True
+        '
+        'SK2
+        '
+        Me.SK2.Location = New System.Drawing.Point(215, 284)
+        Me.SK2.Name = "SK2"
+        Me.SK2.Size = New System.Drawing.Size(88, 45)
+        Me.SK2.TabIndex = 13
+        Me.SK2.Text = "#2"
+        Me.SK2.UseVisualStyleBackColor = True
+        '
+        'SK3
+        '
+        Me.SK3.Location = New System.Drawing.Point(313, 284)
+        Me.SK3.Name = "SK3"
+        Me.SK3.Size = New System.Drawing.Size(88, 45)
+        Me.SK3.TabIndex = 14
+        Me.SK3.Text = "#3"
+        Me.SK3.UseVisualStyleBackColor = True
+        '
+        'SK4
+        '
+        Me.SK4.Location = New System.Drawing.Point(407, 284)
+        Me.SK4.Name = "SK4"
+        Me.SK4.Size = New System.Drawing.Size(88, 45)
+        Me.SK4.TabIndex = 15
+        Me.SK4.Text = "#4"
+        Me.SK4.UseVisualStyleBackColor = True
+        '
+        'SK5
+        '
+        Me.SK5.Location = New System.Drawing.Point(501, 284)
+        Me.SK5.Name = "SK5"
+        Me.SK5.Size = New System.Drawing.Size(88, 45)
+        Me.SK5.TabIndex = 16
+        Me.SK5.Text = "#5"
+        Me.SK5.UseVisualStyleBackColor = True
+        '
+        'SK6
+        '
+        Me.SK6.Location = New System.Drawing.Point(598, 284)
+        Me.SK6.Name = "SK6"
+        Me.SK6.Size = New System.Drawing.Size(88, 45)
+        Me.SK6.TabIndex = 17
+        Me.SK6.Text = "#6"
+        Me.SK6.UseVisualStyleBackColor = True
+        '
+        'SK12
+        '
+        Me.SK12.Location = New System.Drawing.Point(598, 353)
+        Me.SK12.Name = "SK12"
+        Me.SK12.Size = New System.Drawing.Size(88, 45)
+        Me.SK12.TabIndex = 23
+        Me.SK12.Text = "#12"
+        Me.SK12.UseVisualStyleBackColor = True
+        '
+        'SK11
+        '
+        Me.SK11.Location = New System.Drawing.Point(501, 353)
+        Me.SK11.Name = "SK11"
+        Me.SK11.Size = New System.Drawing.Size(88, 45)
+        Me.SK11.TabIndex = 22
+        Me.SK11.Text = "#11"
+        Me.SK11.UseVisualStyleBackColor = True
+        '
+        'SK10
+        '
+        Me.SK10.Location = New System.Drawing.Point(407, 353)
+        Me.SK10.Name = "SK10"
+        Me.SK10.Size = New System.Drawing.Size(88, 45)
+        Me.SK10.TabIndex = 21
+        Me.SK10.Text = "#10"
+        Me.SK10.UseVisualStyleBackColor = True
+        '
+        'SK9
+        '
+        Me.SK9.Location = New System.Drawing.Point(313, 353)
+        Me.SK9.Name = "SK9"
+        Me.SK9.Size = New System.Drawing.Size(88, 45)
+        Me.SK9.TabIndex = 20
+        Me.SK9.Text = "#9"
+        Me.SK9.UseVisualStyleBackColor = True
+        '
+        'SK8
+        '
+        Me.SK8.Location = New System.Drawing.Point(215, 353)
+        Me.SK8.Name = "SK8"
+        Me.SK8.Size = New System.Drawing.Size(88, 45)
+        Me.SK8.TabIndex = 19
+        Me.SK8.Text = "#8"
+        Me.SK8.UseVisualStyleBackColor = True
+        '
+        'SK7
+        '
+        Me.SK7.Location = New System.Drawing.Point(117, 353)
+        Me.SK7.Name = "SK7"
+        Me.SK7.Size = New System.Drawing.Size(88, 45)
+        Me.SK7.TabIndex = 18
+        Me.SK7.Text = "#7"
+        Me.SK7.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.SK12)
+        Me.Controls.Add(Me.SK11)
+        Me.Controls.Add(Me.SK10)
+        Me.Controls.Add(Me.SK9)
+        Me.Controls.Add(Me.SK8)
+        Me.Controls.Add(Me.SK7)
+        Me.Controls.Add(Me.SK6)
+        Me.Controls.Add(Me.SK5)
+        Me.Controls.Add(Me.SK4)
+        Me.Controls.Add(Me.SK3)
+        Me.Controls.Add(Me.SK2)
+        Me.Controls.Add(Me.SK1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ResultLabel)
+        Me.Controls.Add(Me.Result)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Input3)
+        Me.Controls.Add(Me.Input2)
+        Me.Controls.Add(Me.Input1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Input1 As Label
+    Friend WithEvents Input2 As Label
+    Friend WithEvents Input3 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Result As Label
+    Friend WithEvents ResultLabel As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Result.Click
+
+    End Sub
+
+    Friend WithEvents SK1 As Button
+    Friend WithEvents SK2 As Button
+    Friend WithEvents SK3 As Button
+    Friend WithEvents SK4 As Button
+    Friend WithEvents SK5 As Button
+    Friend WithEvents SK6 As Button
+    Friend WithEvents SK12 As Button
+    Friend WithEvents SK11 As Button
+    Friend WithEvents SK10 As Button
+    Friend WithEvents SK9 As Button
+    Friend WithEvents SK8 As Button
+    Friend WithEvents SK7 As Button
+End Class
---
+++ Minggu2/HariKeduaForm/HariKeduaForm/Form1.vb
@@ -0,0 +1,123 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Val(TextBox1.Text) >= 0 Then ResultLabel.Text = "Positif"
+        If Val(TextBox1.Text) < 0 Then ResultLabel.Text = "Negatif"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If Val(TextBox1.Text) > 5 Then ResultLabel.Text = "Lebih dari 5"
+        If Val(TextBox1.Text) < 5 Then ResultLabel.Text = "Kurang dari 5"
+        If Val(TextBox1.Text) = 5 Then ResultLabel.Text = "Sama dengan 5"
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If Val(TextBox1.Text) > 10 Then ResultLabel.Text = "Lebih dari 10"
+        If Val(TextBox1.Text) < 10 Then ResultLabel.Text = "Kurang dari 10"
+        If Val(TextBox1.Text) = 10 Then ResultLabel.Text = "Sama dengan 10"
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Select Case (Val(TextBox1.Text))
+            Case 100
+                ResultLabel.Text = "A"
+            Case 80
+                ResultLabel.Text = "B"
+            Case 70
+                ResultLabel.Text = "C"
+            Case 60
+                ResultLabel.Text = "D"
+            Case 50
+                ResultLabel.Text = "E"
+            Case Else
+                MsgBox("Nilai tidak terdeteksi", , "Peringatan")
+        End Select
+    End Sub
+
+
+
+
+    Private Sub SK1_Click(sender As Object, e As EventArgs) Handles SK1.Click
+        If Val(TextBox1.Text) Mod 2 = 0 Then ResultLabel.Text = "Genap"
+        If Val(TextBox1.Text) Mod 2 = 1 Then ResultLabel.Text = "Ganjil"
+    End Sub
+
+    Private Sub SK2_Click(sender As Object, e As EventArgs) Handles SK2.Click
+        If Val(TextBox1.Text) Mod 3 = 0 Then ResultLabel.Text = "Kelipatan 3"
+        If Val(TextBox1.Text) Mod 3 <> 0 Then ResultLabel.Text = "Bukan Kelipatan 3"
+    End Sub
+
+    Private Sub SK3_Click(sender As Object, e As EventArgs) Handles SK3.Click
+        If Val(TextBox1.Text) >= 70 Then ResultLabel.Text = "Lulus"
+        If Val(TextBox1.Text) < 70 Then ResultLabel.Text = "Tidak Lulus"
+    End Sub
+
+    Private Sub SK4_Click(sender As Object, e As EventArgs) Handles SK4.Click
+        Dim ratarata As Int16 = (Val(TextBox1.Text) + Val(TextBox2.Text)) / 2
+        If (ratarata > 70) Then
+            ResultLabel.Text = "Lulus"
+            BackColor = Color.Blue
+            ResultLabel.ForeColor = Color.Yellow
+        Else
+            ResultLabel.Text = "Tidak Lulus"
+            BackColor = Color.Red
+            ResultLabel.ForeColor = Color.White
+        End If
+    End Sub
+
+    Private Sub SK5_Click(sender As Object, e As EventArgs) Handles SK5.Click
+        If Val(TextBox1.Text) > Val(TextBox2.Text) Then ResultLabel.Text = Val(TextBox1.Text) & "<" & Val(TextBox2.Text)
+        If Val(TextBox1.Text) < Val(TextBox2.Text) Then ResultLabel.Text = Val(TextBox1.Text) & ">" & Val(TextBox2.Text)
+    End Sub
+
+    Private Sub SK6_Click(sender As Object, e As EventArgs) Handles SK6.Click
+        If Val(TextBox1.Text) > Val(TextBox2.Text) Then ResultLabel.Text = Val(TextBox1.Text) & "<" & Val(TextBox2.Text)
+        If Val(TextBox1.Text) < Val(TextBox2.Text) Then ResultLabel.Text = Val(TextBox1.Text) & ">" & Val(TextBox2.Text)
+        If Val(TextBox1.Text) = Val(TextBox2.Text) Then ResultLabel.Text = Val(TextBox1.Text) & "=" & Val(TextBox2.Text)
+
+    End Sub
+
+    Private Sub SK7_Click(sender As Object, e As EventArgs) Handles SK7.Click
+        Dim ratarata As Int16 = (Val(TextBox1.Text) + Val(TextBox2.Text) + Val(TextBox3.Text)) / 2
+        If (ratarata > 70) Then
+            ResultLabel.Text = "Lulus"
+            BackColor = Color.Blue
+            ResultLabel.ForeColor = Color.Yellow
+        Else
+            ResultLabel.Text = "Tidak Lulus"
+            BackColor = Color.Red
+            ResultLabel.ForeColor = Color.White
+        End If
+    End Sub
+
+    Private Sub SK8_Click(sender As Object, e As EventArgs) Handles SK8.Click
+        If Val(TextBox1.Text) >= 81 AndAlso Val(TextBox1.Text) <= 100 Then ResultLabel.Text = "A"
+        If Val(TextBox1.Text) >= 76 AndAlso Val(TextBox1.Text) <= 80 Then ResultLabel.Text = "AB"
+        If Val(TextBox1.Text) < 76 Or Val(TextBox1.Text) > 100 Then ResultLabel.Text = "K"
+    End Sub
+
+    Private Sub SK9_Click(sender As Object, e As EventArgs) Handles SK9.Click
+        Dim ratarata As Int16 = (Val(TextBox1.Text) + Val(TextBox2.Text) + Val(TextBox3.Text)) / 3
+        If ratarata >= 81 AndAlso ratarata <= 100 Then ResultLabel.Text = "A"
+        If ratarata >= 76 AndAlso ratarata <= 80 Then ResultLabel.Text = "AB"
+        If ratarata < 76 Or ratarata > 100 Then ResultLabel.Text = "K"
+    End Sub
+
+    Private Sub SK10_Click(sender As Object, e As EventArgs) Handles SK10.Click
+        If Val(TextBox1.Text) > Val(TextBox2.Text) AndAlso Val(TextBox1.Text) > Val(TextBox3.Text) Then ResultLabel.Text = Val(TextBox1.Text)
+        If Val(TextBox2.Text) > Val(TextBox3.Text) AndAlso Val(TextBox2.Text) > Val(TextBox1.Text) Then ResultLabel.Text = Val(TextBox2.Text)
+        If Val(TextBox3.Text) > Val(TextBox1.Text) AndAlso Val(TextBox3.Text) > Val(TextBox2.Text) Then ResultLabel.Text = Val(TextBox3.Text)
+    End Sub
+
+    Private Sub SK11_Click(sender As Object, e As EventArgs) Handles SK11.Click
+        If Val(TextBox1.Text) < Val(TextBox2.Text) AndAlso Val(TextBox1.Text) < Val(TextBox3.Text) Then ResultLabel.Text = Val(TextBox1.Text)
+        If Val(TextBox2.Text) < Val(TextBox3.Text) AndAlso Val(TextBox2.Text) < Val(TextBox1.Text) Then ResultLabel.Text = Val(TextBox2.Text)
+        If Val(TextBox3.Text) < Val(TextBox1.Text) AndAlso Val(TextBox3.Text) < Val(TextBox2.Text) Then ResultLabel.Text = Val(TextBox3.Text)
+    End Sub
+
+    Private Sub SK12_Click(sender As Object, e As EventArgs) Handles SK12.Click
+        If Val(TextBox1.Text) = Val(TextBox2.Text) Then ResultLabel.Text = "Input 1 dan 2 sama yaitu: " & Val(TextBox1.Text)
+        If Val(TextBox1.Text) = Val(TextBox3.Text) Then ResultLabel.Text = "Input 1 dan 3 sama yaitu: " & Val(TextBox2.Text)
+        If Val(TextBox2.Text) = Val(TextBox3.Text) Then ResultLabel.Text = "Input 2 dan 3 sama yaitu: " & Val(TextBox3.Text)
+    End Sub
+End Class
